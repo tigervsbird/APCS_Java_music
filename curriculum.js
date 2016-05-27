660a54a56fd0872c3ef4d8fdec5f36d574074fea
Truman Nguyen <truman.n@bronxsoftware.org>
2016-05-27 09:47:07 -0400

    Update curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -13,17 +13,24 @@
 init();
 setTempo(140);
 
-for (var i = 0; i < 2; i++) {
+
 
 fitMedia(DUBSTEP_BASS_WOBBLE_013, 1, 1, 2); // Each kick drum hit lasts a quarter note: 1/4 of a measure.
 fitMedia(DUBSTEP_BASS_WOBBLE_016, 2, 1, 2);
 fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 3, 1, 2);
 fitMedia(TIGERVSBIRD_BE_FREE_MY_CHILDREN, 4, 1, 2);
-//fitMedia(RD_EDM_HIT_LEAD_1, 5, 1, 2);
-}//Music Section
-//fitMedia(DUBSTEP_BASS_WOBBLE_013, 1, 1, 2); // Each kick drum hit lasts a quarter note: 1/4 of a measure.
-//fitMedia(DUBSTEP_BASS_WOBBLE_016, 2, 1, 2);
-//fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 3, 1, 2);
+fitMedia(DUBSTEP_BASS_WOBBLE_013, 1, 2, 3); // Each kick drum hit lasts a quarter note: 1/4 of a measure.
+fitMedia(DUBSTEP_BASS_WOBBLE_016, 2, 2, 3);
+fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 3, 2, 3);
+fitMedia(TIGERVSBIRD_BE_FREE_MY_CHILDREN, 4, 2, 3);
+fitMedia(ELECTRO_ANALOGUE_BASS_001, 1, 3, 8);//fitMedia(RD_EDM_HIT_LEAD_1, 5, 1, 2);
+ // Each kick drum hit lasts a quarter note: 1/4 of a measure.
+fitMedia(DUBSTEP_BASS_WOBBLE_016, 2, 3, 8);
 
+fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 3, 3, 8);
+fitMedia(DUBSTEP_BASS_WOBBLE_013, 1, 8, 9); // Each kick drum hit lasts a quarter note: 1/4 of a measure.
+fitMedia(DUBSTEP_BASS_WOBBLE_016, 2, 8, 9);
+fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 3, 8, 9);
+fitMedia(TIGERVSBIRD_BE_FREE_MY_CHILDREN, 4, 8, 9);
 //Finish Section
 finish();
